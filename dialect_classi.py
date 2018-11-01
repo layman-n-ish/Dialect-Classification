a805b9adc7c416b74f2bf4b438e3e9a40e50e88d
Nishant Malpani <Nishant.Malpani@iiitb.org>
2018-11-01 20:24:34 +0530

    Done with a tad of Feature extraction and training

--- dialect_classi.py
+++ dialect_classi.py
@@ -9,6 +9,14 @@ from python_speech_features import mfcc
 from python_speech_features import delta
 from python_speech_features import logfbank
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+import xgboost as xgb
+from sklearn.ensemble import RandomForestClassifier
+
+import warnings
+
 dialects = ["IDR1", "IDR2", "IDR3", "IDR4", "IDR5", 
 "IDR6", "IDR7", "IDR8", "IDR9"]
 
@@ -39,16 +47,33 @@ def audio_plot(audio_file):
     ax[1].set_ylabel('|Y(freq)|')
 
 def feature_engineering(audio, Fs):
-    features = {}
+    features = []
+    
+    mfcc_coeff = mfcc(audio, Fs, winlen=0.023)
+    fbank_coeff = logfbank(audio, Fs, winlen=0.023)
+    delta_mfcc_coeff = delta(mfcc_coeff, 2)
 
-    #stft
-    #mfcc
-    #chroma vector, deviation
-    #spectral_contrast
-    #tonnetz
-    #zero crossing rate
-    #spectral centroid
-    #spectral flux
+    tot_frames = mfcc_coeff.shape[0]
+    mfcc_coeffs_len = mfcc_coeff.shape[1]
+    fbank_coeffs_len = fbank_coeff.shape[1]
+
+    for j in range(mfcc_coeffs_len):
+        mfcc_coeffs = []
+        delta_coeffs = []
+        for i in range(tot_frames):
+            mfcc_coeffs.append(mfcc_coeff[i][j])
+            delta_coeffs.append(delta_mfcc_coeff[i][j])
+        features.append(np.mean(mfcc_coeffs))
+        features.append(np.var(mfcc_coeffs))
+        features.append(np.mean(delta_coeffs))
+        features.append(np.var(delta_coeffs))
+
+    for j in range(fbank_coeffs_len):
+        fbank_coeffs = []
+        for i in range(tot_frames):
+            fbank_coeffs.append(fbank_coeff[i][j])    
+        features.append(np.mean(fbank_coeffs))
+        features.append(np.var(fbank_coeffs))
 
     return features
 
@@ -58,21 +83,73 @@ def get_audio_files(dialect_class):
     return audio_files
 
 def prep_data():
-    data = []
-    labels = []
+    data = {}
 
+    i = 0
     for dialect_class in dialects:
-        print("Prepping data for class %s" %(dialect_class))
+        print("\nPrepping data for class %s" %(dialect_class))
         audio_files = get_audio_files(dialect_class)
 
         for file in audio_files:
             aud, Fs = lr.load(file)
-            feature_dict = feature_engineering(aud, Fs)
+            feature_arr = feature_engineering(aud, Fs)
+            feature_arr.append(dialects.index(dialect_class))
+            data[i] = feature_arr
+            i = i+1
 
-            data.append(feature_dict)
-            label.append(dialects.index(dialect_class))
+    X = pd.DataFrame.from_dict(data, orient='index')
+    X.to_csv('X_new.csv')
+    print("\nDone making the CSV!\n")
 
-    return data, labels
-
-#def train_model():
+def train_model():
+    data = pd.read_csv('X_new.csv')
+    train, test = train_test_split(data, test_size=0.33, random_state=1, shuffle=True)
     
+    l = []
+    for i in range(0, 104):
+        l.append(str(i))
+
+    X_train = train.loc[:, l]
+    y_train = train.loc[:, '104']   
+    X_test = test.loc[:, l]
+    y_test = test.loc[:, '104']
+
+    #print(X_train.head())
+    #print(y_train.head())
+    
+    train_rf(X_train, y_train, X_test, y_test)
+    train_xgb(X_train, y_train, X_test, y_test)
+    train_lr(X_train, y_train, X_test, y_test)
+
+def train_lr(X_train, y_train, X_test, y_test):
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.fit_transform(X_test)
+
+    model = LogisticRegression(C=2, solver='newton-cg', n_jobs=-1, tol=0.01)
+    print("\nLogistic Regression: Training...")
+    model.fit(X_train, y_train)
+    print("\nLogistic Regression: Training score: %f"%(model.score(X_train, y_train)))
+    print("\nLogistic Regression: Test score: %f"%(model.score(X_test, y_test)))
+
+def train_rf(X_train, y_train, X_test, y_test):
+
+    model = RandomForestClassifier(n_estimators=400, max_depth=20, min_samples_split=5, min_samples_leaf=5)
+    print("\nRandom Forest: Training...")
+    model.fit(X_train, y_train)
+    print("\nRandom Forest: Training score: %f"%(model.score(X_train, y_train)))
+    print("\nRandom Forest: Test score: %f"%(model.score(X_test, y_test)))
+
+def train_xgb(X_train, y_train, X_test, y_test):
+
+    model = xgb.XGBClassifier(n_estimators=200, learning_rate=0.5)
+    print("\nXGBoost: Training...")
+    model.fit(X_train, y_train)
+    print("\nXGBoost: Training score: %f"%(model.score(X_train, y_train)))
+    print("\nXGBoost: Test score: %f"%(model.score(X_test, y_test)))
+
+    
+if __name__ == "__main__":
+    warnings.filterwarnings(action='ignore', category=DeprecationWarning)
+    train_model()
+  
